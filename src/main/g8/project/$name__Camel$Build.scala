95ca1f6c03c4f2d088435fa913ef3b1a47b56538
Daniel Schobel <dschobel@gmail.com>
2013-04-24 11:44:46 -0400

    make 2.10.1 the default add scalatest dep

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -11,6 +11,7 @@ object $name;format="Camel"$Build extends Build {
       organization := "$organization$",
       version := "$version$",
       scalaVersion := "$scala_version$"
+      libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
       // add other settings here
     )
   )
